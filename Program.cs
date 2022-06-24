605541ff10ff25c6f513d8635378045e01eba79e
Abdulrahman-Alkhaldi <hamanycore@gmail.com>
2022-06-24 17:11:26 +0300

    HW 1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string hero = "abdo"; // why ask us to create this if we wont even use it? :D
+int health = 100;
+
+int damaged()
+{
+    return health -= 20;
+}
+int boost()
+{
+    return health += 10;
+}
+int damagedHealth = damaged();
+int boostedHealth = boost();
+Console.WriteLine($"Damaged Health: {damagedHealth}");
+Console.WriteLine($"Boosted Health: {boostedHealth}");
+if (damagedHealth < boostedHealth){
+    Console.WriteLine(boostedHealth);
+    Console.WriteLine($"{damagedHealth} is less than {boostedHealth}");
+} else if (damagedHealth > boostedHealth){
+    Console.WriteLine(damagedHealth);
+    Console.WriteLine($"{damagedHealth} is greater than {boostedHealth}");
+} else {
+    Console.WriteLine($"{damagedHealth} is equal to {boostedHealth}");
+}
